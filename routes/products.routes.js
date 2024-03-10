235fecaec4e10ea6df9dbd3d1d8fc68b85e5b74f
Nicolas Gruger <nicolas.gruger@hotmail.com>
2024-03-10 10:10:48 -0300

    Se actualizó el archivo de rutas products.routes.js eliminando las subrutas de productos

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,8 +4,4 @@ const productsController = require('../controllers/productsControllers');
 
 router.get('/', productsController.products);
 
-router.get('/:idProduct', productsController.products);
-
-router.get('/:idProduct/comentarios/:idComentario?', productsController.products);
-
 module.exports = router;
